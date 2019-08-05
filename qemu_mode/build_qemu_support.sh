7c151c8441b20522dc07b5144b8653a49ecec587
Pietro Ferretti <pietro.ferretti1@gmail.com>
2019-08-05 17:02:16 +0200

    Fix compilation errors for QEMU mode for recent glibc (#10)
    * Fix compilation errors for QEMU mode for recent glibc
    See QEMU git commit 75e5b70e6b5dcc4f2219992d7cffa462aa406af0
    
    * Added QEMU mode to Travis

--- qemu_mode/build_qemu_support.sh
+++ qemu_mode/build_qemu_support.sh
@@ -137,6 +137,8 @@ echo "[*] Applying patches..."
 patch -p1 <../patches/elfload.diff || exit 1
 patch -p1 <../patches/cpu-exec.diff || exit 1
 patch -p1 <../patches/syscall.diff || exit 1
+patch -p1 <../patches/configure.diff || exit 1
+patch -p1 <../patches/memfd.diff || exit 1
 
 echo "[+] Patching done."
 
