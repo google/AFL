56f510190df53942006b0996590aa6e2cfb153b7
Alexander Bayandin <a.bayandin@gmail.com>
2019-09-25 18:55:01 +0100

    Prepare for v2.56b release

--- config.h
+++ config.h
@@ -28,7 +28,7 @@
 
 /* Version string: */
 
-#define VERSION             "2.54b"
+#define VERSION             "2.56b"
 
 /******************************************************
  *                                                    *
