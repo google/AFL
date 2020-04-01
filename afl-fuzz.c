8c8795fb14ad05a22df2a652e1b52a4ff0759be3
Wolfram Rösler <wolfram@roesler-ac.de>
2020-04-01 16:53:37 +0200

    Add option -V to afl-fuzz: Show version number and exit

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -7107,7 +7107,8 @@ static void usage(u8* argv0) {
 
        "  -T text       - text banner to show on the screen\n"
        "  -M / -S id    - distributed mode (see parallel_fuzzing.txt)\n"
-       "  -C            - crash exploration mode (the peruvian rabbit thing)\n\n"
+       "  -C            - crash exploration mode (the peruvian rabbit thing)\n"
+       "  -V            - show version number and exit\n\n"
 
        "For additional tips, please consult %s/README.\n\n",
 
@@ -7764,7 +7765,7 @@ int main(int argc, char** argv) {
   gettimeofday(&tv, &tz);
   srandom(tv.tv_sec ^ tv.tv_usec ^ getpid());
 
-  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Q")) > 0)
+  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:QV")) > 0)
 
     switch (opt) {
 
@@ -7931,6 +7932,11 @@ int main(int argc, char** argv) {
         if (!mem_limit_given) mem_limit = MEM_LIMIT_QEMU;
 
         break;
+
+      case 'V': /* Show version number */
+
+        /* Version number has been printed already, just quit. */
+        exit(0);
 
       default:
 
